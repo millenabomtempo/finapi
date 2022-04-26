44206a5401c2712f17c27fedd4a086f9a614b490
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-04-26 14:55:01 -0300

    Criando saque na conta

--- src/index.js
+++ src/index.js
@@ -29,6 +29,18 @@ function verifyIfExistsAccountCPF(request, response, next) {
   return next()
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === 'credit') {
+      return acc + operation.amount
+    } else {
+      return acc - operation.amount
+    }
+  }, 0)
+
+  return balance
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
 
@@ -71,4 +83,25 @@ app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
   return response.status(201).send()
 })
 
-app.listen(3333)
+app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body
+  const { customer } = request
+
+  const balance = getBalance(customer.statement)
+
+  if (balance < amount) {
+    return response.status(400).json({ error: 'Insufficient funds!' })
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: 'debit'
+  }
+
+  customer.statement.push(statementOperation)
+
+  return response.status(201).send()
+})
+
+app.listen(3333) 
